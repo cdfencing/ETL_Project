a75291e661b11cfe774f59d94b84d73112a8de0b
cdfencing <65666202+cdfencing@users.noreply.github.com>
2020-08-29 12:07:05 -0500

    project work

---
+++ tablecreation.sql
@@ -0,0 +1,21 @@
+CREATE TABLE country_happiness_index (
+country TEXT PRIMARY KEY,
+happiness_rank INT,
+life_expectancy FLOAT,
+freedom FLOAT,
+gdp_pc_usdollar FLOAT,
+suicide_rate FLOAT	
+);
+
+CREATE TABLE country_alcohol_consumption (
+country TEXT PRIMARY KEY,
+beer_servings INT,
+spirit_servings INT,
+wine_servings INT,
+total_litres_of_pure_alcohol FLOAT
+);
+
+CREATE TABLE country_obesity (
+country TEXT PRIMARY KEY,
+obesity_pct FLOAT
+);
